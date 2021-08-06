3ede1fd3f2db5910e0386152d2d4a3ee4adc768e
viiiraj07 <avisec07@gmail.com>
2021-08-06 18:28:51 +0530

    aws -> heroku

--- Newsletter/form.js
+++ Newsletter/form.js
@@ -15,7 +15,7 @@ document.getElementById("submit").addEventListener("click", (e) => {
     e.preventDefault();
 
 
-    var url = "http://antisloth-env-1.eba-2vq6nuns.ap-south-1.elasticbeanstalk.com/form/save";
+    var url = "https://anti-sloth.herokuapp.com/form/save";
 
     var xhr = new XMLHttpRequest();
 
