93d92ea1825186b1d72398c1e3e3c32130124405
viiiraj07 <avisec07@gmail.com>
2021-08-06 17:27:59 +0530

    newsletter

---
+++ Newsletter/form.js
@@ -0,0 +1,28 @@
+document.getElementById("submit").addEventListener("click", (e) => {
+    var email = document.getElementById("email").value;
+
+
+    if (email.length == 0) {
+        alert("Enter correct email!");
+        return;
+    }
+
+    e.preventDefault();
+
+
+    var url = "http://antisloth-env-1.eba-2vq6nuns.ap-south-1.elasticbeanstalk.com/form/save";
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", url);
+
+    xhr.setRequestHeader("Accept", "application/json");
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    var data = `{
+        "email": "${email}"
+    }`;
+
+    xhr.send(data);
+});
